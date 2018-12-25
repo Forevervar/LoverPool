72b728d0bd8d15d21b214cc75140dbcd12ad4a42
Nero <Nero@Nero-PC>
2018-12-25 20:31:03 +0800

    first commit

---
+++ src/main/java/com/nero/loverpool/LoverpoolApplication.java
@@ -0,0 +1,14 @@
+package com.nero.loverpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoverpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoverpoolApplication.class, args);
+	}
+
+}
+
